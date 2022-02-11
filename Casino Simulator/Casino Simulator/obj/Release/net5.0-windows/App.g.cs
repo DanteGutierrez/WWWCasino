31855bee5cedf1e1268aa128a637731f64509efb
culintim000 <cultim000@gmail.com>
2022-02-10 18:50:22 -0700

    Maybe Git ignore works

--- Casino Simulator/Casino Simulator/obj/Release/net5.0-windows/App.g.cs
+++ Casino Simulator/Casino Simulator/obj/Release/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Casino_Simulator {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Casino_Simulator {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public static void Main() {
             Casino_Simulator.App app = new Casino_Simulator.App();
             app.InitializeComponent();
