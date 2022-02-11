5db1c98fab288ad4f49fe0deb893bf7416546f76
Andre <asummers@student.neumont.edu>
2022-02-10 19:07:51 -0700

    Testing making new pages.

---
+++ Casino Simulator/Casino Simulator/Views/Blackjack.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Casino_Simulator.Views
+{
+    public partial class Blackjack : Window
+    {
+        public Blackjack()
+        {
+            InitializeComponent();
+        }
+    }
+}
